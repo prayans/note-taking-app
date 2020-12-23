d288588db0e8b770952f950c9d8c957a1e7b887a
prayans <tyagipriyanshu077@gmail.com>
2020-12-23 17:36:15 +0530

    Updates

---
+++ precache-manifest.17a39573f53b204ddeece042fc67fc50.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e7209d1b053da8c194b1af425434af",
+    "url": "/note-taking-app/index.html"
+  },
+  {
+    "revision": "b10944ae48fd8ef6a8de",
+    "url": "/note-taking-app/static/js/2.53a2afd4.chunk.js"
+  },
+  {
+    "revision": "20112a131db78c5e234b",
+    "url": "/note-taking-app/static/js/main.445c8d8c.chunk.js"
+  },
+  {
+    "revision": "78aa7aa4c480c78a3e7c",
+    "url": "/note-taking-app/static/js/runtime-main.d63271c2.js"
+  }
+]);
